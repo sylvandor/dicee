652125cd232b35704d4fe50bc790fe4c41f777e3
sylvandor <scottwwj@gmail.com>
2019-04-23 16:05:46 -0500

    Initial commit with project started

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Scott Johnson on 4/22/19.
+//  Copyright © 2019 Scott Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
